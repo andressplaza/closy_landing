41c15a84b6a7bb15123e1470dc48f92282f99c5e
andressplaza <andvic.1eg1314@gmail.com>
2025-04-01 00:05:12 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero/Hero';
 import PoliticaPrivacidad from './components/Legal/Politica/PoliticaPrivacidad';
 import TerminosCondiciones from './components/Legal/Terminos/TerminosCondiciones';
-import EliminarCuenta from './components/Legal/EliminarCuenta/EliminarCuenta';
 import 'font-awesome/css/font-awesome.min.css';
 
 
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<Hero />} />
         <Route path="/privacidad" element={<PoliticaPrivacidad />} />
         <Route path="/terminos-condiciones" element={<TerminosCondiciones />} />
-        <Route path="/eliminar-cuenta" element={<EliminarCuenta />} />
       </Routes>
     </Router>
   );
